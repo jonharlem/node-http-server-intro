a0adb96d051c9c0b437103ef704db91a9eedbb77
Jon Harlem <jtharlem@gmail.com>
2015-12-04 15:12:32 -0800

    added index and route file

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 var http = require('http');
-var fs   = require('fs');
+var fs = require('fs');
 
 var PORT = 8000;
 
 function handleRequest(req, res) {
-  res.setHeader("Content-Type", "text/html");
-  res.statusCode = 200;
-  res.write("<h1>I can change the response</h1>");
-  res.end();
-};
+	if (req.url === '/index.html') {
+		fs.readFile('./index.html', function(err, data) {
+			if (err) {
+				throw err;
+			}
+
+			res.setHeader("Content-Type", "text/html");
+			res.statusCode = 200;
+			res.write(req.url);
+			res.end();
+		});
+	}
+}
 
 var server = http.createServer(handleRequest);
 
