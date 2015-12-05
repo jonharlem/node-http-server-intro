c8661d0de48bb21e4261da3d15958aae59097ea1
Jon Harlem <jtharlem@gmail.com>
2015-12-04 16:46:37 -0800

    added routes, still need to finish

--- routes.js
+++ routes.js
@@ -1,1 +1,16 @@
-routes.js
+var fs = require('fs');
+
+routes = {
+	'/index.html': function(req, res) {
+		fs.readFile('.index.html', function(err, data) {
+			if(err) {
+				throw err;
+			}
+		res.setHeader("Content-Type", "text/html");
+		res.write(data)
+		res.end('index.html')
+	})
+	}
+}
+
+module.exports = routes
--- server.js
+++ server.js
@@ -1,21 +1,17 @@
 var http = require('http');
 var fs = require('fs');
+var routes = require('./routes')
 
 var PORT = 8000;
 
 function handleRequest(req, res) {
-	if (req.url === '/index.html') {
-		fs.readFile('./index.html', function(err, data) {
-			if (err) {
-				throw err;
-			}
-
-			res.setHeader("Content-Type", "text/html");
-			res.statusCode = 200;
-			res.write(req.url);
-			res.end();
-		});
-	}
+	if (routes[req.url]) {
+		routes[req.url](req, res)
+	} else {
+		res.statusCode = 404;
+		res.end("This doesn't exisstatu")
+	});
+}
 }
 
 var server = http.createServer(handleRequest);
@@ -23,3 +19,13 @@ var server = http.createServer(handleRequest);
 server.listen(PORT, function() {
 	console.log("I'm listening on port " + PORT)
 });
+
+// fs.readFile('./index.html', function(err, data) {
+// 	if (err) {
+// 		throw err;
+// 	}
+
+// 	res.setHeader("Content-Type", "text/html");
+// 	res.statusCode = 200;
+// 	res.write(data);
+// 	res.end();
