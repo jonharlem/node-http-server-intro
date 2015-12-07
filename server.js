e4b2b5eee0a0c19e1e2a1291937346b0fa842fd2
Jon Harlem <jtharlem@gmail.com>
2015-12-07 14:30:21 -0800

    fixed routes, formatted server code, and added 'an' to about.html

--- server.js
+++ server.js
@@ -6,13 +6,12 @@ var PORT = 8000;
 
 function handleRequest(req, res) {
 	if (routes[req.url]) {
-		routes[req.url](req, res)
+		routes[req.url](req, res);
 	} else {
 		res.statusCode = 404;
-		res.end("This doesn't exisstatu")
-	});
-}
-}
+		res.end("This doesn't exist");
+	}
+};
 
 var server = http.createServer(handleRequest);
 
