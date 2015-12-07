68db626b861d197dece8a3c7b73b5e3ebd2029b3
Jon Harlem <jtharlem@gmail.com>
2015-12-07 15:41:29 -0800

    added 'page' to h1 on about.html

--- routes.js
+++ routes.js
@@ -1,6 +1,16 @@
 var fs = require('fs');
 
 routes = {
+	'/': function(req, res) {
+		fs.readFile('./home.html', function(err, data) {
+			if (err) {
+				throw err;
+			}
+			res.setHeader("Content-Type", "text/html");
+			res.write(data);
+			res.end();
+		})
+	},
 	'/index.html': function(req, res) {
 		fs.readFile('./index.html', function(err, data) {
 			if (err) {
